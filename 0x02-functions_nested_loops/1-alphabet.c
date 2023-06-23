d704ee61550cae6310064a9956a87b2a8cc43119
deekenyah1 <kdianneefua@gmail.com>
2023-06-23 11:32:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'r'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
