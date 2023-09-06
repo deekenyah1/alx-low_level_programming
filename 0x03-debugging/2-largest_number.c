3e92681dbe0ebd35c174f80bafb3834eea4eb20c
deekenyah1 <kdianneefua@gmail.com>
2023-09-06 05:16:51 +0000

    2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,24 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
+	else if (a > c && c > b)
 	{
-		largest = c;
+		largest = a;
 	}
-	else if (b > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	else
+	else if (b > c && c > a)
+	{
+		largest = b;
+	}
+	else if (c > a && a > b)
 	{
 		largest = c;
 	}
+	else
+		largest = c;
+
 	return (largest);
 }
